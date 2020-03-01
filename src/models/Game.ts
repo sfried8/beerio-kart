1dfaee6e594d253a861fc235848adecafcc03842
Sam Friedman <sfried8@gmail.com>
2020-03-01 15:14:49 -0500

    minor code cleanup

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -16,7 +16,6 @@ import Vue from "vue";
 import DatabaseManager from "../MongoDatabaseManager";
 import { IGameData } from "@/DatabaseManager";
 export default class Game {
-    public raceResults: number[];
     public roundNumber: number = -1;
     public datasets: { x: number; y: number }[][] = [];
     public _id?: string;
@@ -26,7 +25,6 @@ export default class Game {
         public history: number[][] = [],
         existingId?: string
     ) {
-        this.raceResults = [];
         this.datasets = players.map(p => []);
         if (existingId) {
             this._id = existingId;
@@ -36,46 +34,23 @@ export default class Game {
         }
     }
     async init() {
-        return DatabaseManager.newGame({
+        this._id = await DatabaseManager.newGame({
             players: this.players.map(({ _id: id }) => "" + id!),
             numRaces: this.numRaces,
             history: this.history,
             date: new Date()
-        }).then(id => {
-            this._id = id;
-            // this.players.forEach((p, i) => {
-            //     DatabaseManager.getDataPointsByPlayer(p._id || "").then(dps => {
-            //         Vue.set(
-            //             this.datasets,
-            //             i,
-            //             dps
-            //                 .filter(dp => dp.gameId != this._id)
-            //                 .map(({ x, y }) => ({ x, y }))
-            //         );
-            //     });
-            // });
         });
     }
     repopulateHistory() {
         this.startGame();
         for (const historyItem of this.history) {
-            this.raceResults = [...historyItem];
-            this.round(true);
+            this.round([...historyItem], true);
         }
     }
     startGame() {
         this.roundNumber = 0;
 
         this.datasets.forEach(d => d.splice(0, d.length));
-        // this.players.forEach((p, i) => {
-        //     DatabaseManager.getDataPointsByPlayer(p._id || "").then(dps => {
-        //         Vue.set(
-        //             this.datasets,
-        //             i,
-        //             dps.map(({ x, y }) => ({ x, y }))
-        //         );
-        //     });
-        // });
         this.players.forEach((p, i) => {
             p.currentRoundPoints.clear();
             p.extraDict = [];
@@ -84,14 +59,14 @@ export default class Game {
             p.numPlayers = this.players.length;
         });
     }
-    round(isUndoing: boolean = false) {
+    round(raceResults: number[], isUndoing: boolean = false) {
         this.roundNumber += 1;
 
-        const lastPlace = Math.max(...this.raceResults);
+        const lastPlace = Math.max(...raceResults);
         for (let i = 0; i < this.players.length; i++) {
             const p = this.players[i];
 
-            const addedPoints = this.raceResults[i];
+            const addedPoints = raceResults[i];
             const dataPoint = { x: p.totalPoints(), y: addedPoints };
 
             if (!isUndoing && this._id && p._id) {
@@ -102,7 +77,6 @@ export default class Game {
                 });
             }
             this.datasets[i].push(dataPoint);
-            this.raceResults[i] = 0;
             p.points.combineGroup(p.currentRoundPoints);
             p.currentRoundPoints.clear();
             p.addRacePoints(addedPoints, lastPlace);
@@ -138,23 +112,18 @@ export default class Game {
         printScores(this.players);
     }
     saveGame() {
-        // const iGameData: IGame = {
-        //     id: this.id,
-        //     players: this.players.map(({ id }) => id!),
-        //     numRaces: this.numRaces,
-        //     history: this.history
-        // };
         if (this._id) {
             DatabaseManager.updateGameHistory(this._id, this.history);
         }
     }
     async promptAll() {
+        const raceResults = [];
         for (let i = 0; i < this.players.length; i++) {
             const p = this.players[i];
-            this.raceResults[i] = await KeypadPrompt(p.name);
+            raceResults[i] = await KeypadPrompt(p.name);
         }
-        this.history.push([...this.raceResults]);
-        this.round();
+        this.history.push([...raceResults]);
+        this.round(raceResults);
     }
     get kanpaiPoints() {
         if (+this.numRaces <= 4) {
