0325298ba06f090423c96cad6b429804dad96f0f
Sam Friedman <sfried8@gmail.com>
2020-03-16 20:54:33 -0400

    removed options

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,8 @@ module.exports = {
         themeColor: "#1976d2",
         msTileColor: "#000000",
         appleMobileWebAppCapable: "yes",
-        appleMobileWebAppStatusBarStyle: "black",
+        appleMobileWebAppStatusBarStyle: "black"
 
         // configure the workbox plugin
-        workboxPluginMode: "InjectManifest",
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: "dev/sw.js"
-            // ...other Workbox options...
-        }
     }
 };
